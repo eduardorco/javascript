c2d65c629d3f9e11c1565f7be7926172d50a188c
eduardorco <eduardorco10@gmail.com>
2024-11-01 17:04:51 -0300

    Section 5

--- Section 4/app.js
+++ Section 4/app.js
@@ -1,15 +1,31 @@
-let name = 'Max';
+// VAR x LET
 
-if (name === 'Max') {
-  let hobbies = ['Sports', 'Cooking'];
-  console.log(hobbies);
+// VAR extrapola as funções, causando confusão no script. Por isso hoje em dia se usa let, que respeita seu local
+
+// let name = 'MAX';
+// Se eu redeclarar a variavel "name" fora da função, da erro
+
+// function greet() {
+//     let age = 30;
+// Se eu redeclarar a variavel "name" dentro da função, ela modará apenas aqui
+// let name = "George";
+// console.log(name, age);
+// }
+
+// Se eu tentar chamar age fora da função, vai dar merda
+// console.log(name,age)
+// console.log(name)
+// greet();
+
+// Ao criar variaveis dentro de {} (Menos em casos de objeto) com LET ou CONST, elas só serão validas dentro daquele bloco
+
+function getName() {
+  return prompt('Your name: ', '');
 }
 
 function greet() {
-  let age = 30;
-  let name = 'Gregor';
-  console.log(name, age);
+  const UserName = getName();
+  console.log('Hello ' + UserName);
 }
 
-console.log(name, hobbies);
 greet();
